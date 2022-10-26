b2e54056db7b951172c7d3abd9af810d83ee17ef
leodjtavares <116121793+leodjtavares@users.noreply.github.com>
2022-10-26 10:10:12 +0100

    Update lessons.js

--- js/lessons.js
+++ js/lessons.js
@@ -5,12 +5,12 @@ const lessonsArray =
     "english": [1002, "English", 100, 5, "London", "Example Image"],
     "french": [1003, "French", 100, 5, "London", "Example Image"],
     "spanish": [1004, "Spanish", 100, 5, "London", "Example Image"],
-    "example": [1000, "Example Subject", 100, 5, "Example Location", "Example Image"],
-    "example": [1000, "Example Subject", 100, 5, "Example Location", "Example Image"],
-    "example": [1000, "Example Subject", 100, 5, "Example Location", "Example Image"],
-    "example": [1000, "Example Subject", 100, 5, "Example Location", "Example Image"],
-    "example": [1000, "Example Subject", 100, 5, "Example Location", "Example Image"],
-    "example": [1000, "Example Subject", 100, 5, "Example Location", "Example Image"],
+    "example": [1005, "Example Subject", 100, 5, "Example Location", "Example Image"],
+    "example": [1006, "Example Subject", 100, 5, "Example Location", "Example Image"],
+    "example": [1007, "Example Subject", 100, 5, "Example Location", "Example Image"],
+    "example": [1008, "Example Subject", 100, 5, "Example Location", "Example Image"],
+    "example": [1009, "Example Subject", 100, 5, "Example Location", "Example Image"],
+    "example": [1010, "Example Subject", 100, 5, "Example Location", "Example Image"],
 };
 
 const lessons = JSON.parse(lessonsArray);
