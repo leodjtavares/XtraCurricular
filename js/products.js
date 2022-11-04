382fef890ea75a8d67984a9077611da630c14cf1
leodjtavares <116121793+leodjtavares@users.noreply.github.com>
2022-11-04 19:28:27 +0000

    Refactoring products
    list rendering products to allow multiple products to display

---
+++ js/products.js
@@ -0,0 +1,38 @@
+let product = [
+    {
+        id: 1001,
+        activity: 'Maths Revision',
+        location: 'London',
+        price: 100.00,
+        availableSpaces: 5,
+        rating: 2,
+
+    },
+
+    {
+        id: 1002,
+        activity: "Science Revision",
+        location: "A 25 pound …",
+        price: 20.00,
+        availableSpaces: 10,
+        rating: 2,
+    },
+
+    {
+        id: 1003,
+        activity: "English Revision",
+        location: "Manchester",
+        price: 150.00,
+        availableSpaces: 10,
+        rating: 2,
+    },
+
+    {
+        id: 1004,
+        activity: "Playground Football",
+        location: "Totthenham",
+        price: 5.00,
+        availableSpaces: 20,
+        rating: 1,
+    },
+]
